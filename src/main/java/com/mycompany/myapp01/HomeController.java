2deee80b6abf26404b16d945c0c1dcd242049c7e
KimSongEun <songeun4939@naver.com>
2021-11-06 05:41:16 +0900

    211104 수업_Spring pom.xml, servlet-context.xml, HomeController 실습, spring mvc 구조 학습

--- src/main/java/com/mycompany/myapp01/HomeController.java
+++ src/main/java/com/mycompany/myapp01/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -36,4 +37,43 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(value = "hello2", method = RequestMethod.GET)
+	public String hello2() {
+		return "hello";
+	} // 페이지만 전달
+	
+	@RequestMapping(value = "hello", method = RequestMethod.GET)
+	public ModelAndView hello(ModelAndView mv) {
+		mv.setViewName("hello");
+		mv.addObject("k1", "aaaa");
+		return mv;
+	} // 페이지 + 데이터 전달
+	
+	@RequestMapping(value = "hello3", method = RequestMethod.GET)
+	public String hello3(Model m) {
+		m.addAttribute("k1", "2번째 값전달");
+		return "hello";
+	} // 페이지 + 데이터 전달
+	
+	@RequestMapping(value = "hello4", method = RequestMethod.GET)
+	public ModelAndView hello() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("hello");
+		mv.addObject("k1", "3번째 방법");
+		return mv;
+	} // 페이지 + 데이터 전달
+	
+	@RequestMapping(value = "/aaa", method = RequestMethod.GET)
+	public String adlfjwoasdf(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = "aaaaa";
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
 }
